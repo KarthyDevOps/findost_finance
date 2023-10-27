8717ee4e040b0d14df8f5d7b016194e61fab114c
Gowrisankar <102644027+gowrisankardoodleblue@users.noreply.github.com>
2023-10-27 15:50:43 +0530

    Update korp.controller.js

--- app/controllers/korp.controller.js
+++ app/controllers/korp.controller.js
@@ -1,5 +1,6 @@
 const excelJs = require("exceljs");
 const moment = require("moment");
+const { statusCodes } = require("../response/httpStatusCodes");
 
 const {
     sendErrorResponse,
@@ -348,6 +349,33 @@ const {
         result?.message,
         result?.data
       );
+    }
+    if(params.export == true || params.export == 'true')
+    {
+      let workbook = new excelJs.Workbook();
+      let worksheet = workbook.addWorksheet("Sheet1");
+      worksheet.columns = [
+          { header: "AccountID", key: "AccountID", width: 15 },
+          { header: "AccountName", key: "AccountName", width: 25 },
+          { header: "turnOver", key: "turnOver", width: 25 },
+          { header: "revenue", key: "revenue", width: 25 },
+          { header: "myBrokerageRevenue", key: "myBrokerageRevenue", width: 25 },
+      ];
+
+      let workData = result.data.list || [];
+      console.log(workData)
+     // if (workData?.length) workData = workData?.map(w => ({ ...w, fullName: w?.userDetails?.fullName || "" }))
+      worksheet.addRows(workData);
+      res.setHeader(
+          "Content-Type",
+          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+      );
+      res.setHeader(
+          "Content-Disposition",
+          "attachment; filename=" + `My Reports-${moment().format('YYYY-MM-DD-hh-mm-ss')}.xlsx`
+      );
+      await workbook.xlsx.write(res);
+      return res.status(statusCodes.HTTP_OK).end();
     }
     return sendSuccessResponse(
       req,

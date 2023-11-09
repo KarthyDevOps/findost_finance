c085743d8cca7eae657342d55c066d7d65e4a05f
Gowrisankar <102644027+gowrisankardoodleblue@users.noreply.github.com>
2023-11-09 11:06:04 +0530

    Create cmsIpoDates.js

---
+++ app/models/cmsIpoDates.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+const { getImageURL } = require("../utils/s3Utils")
+const moment = require("moment");
+
+const cmsIpoDatesSchema = new mongoose.Schema(
+    {
+        ipoisinNumber: {
+            type: String,
+            trim: true
+        },
+        ipoDoc: {
+            type: String,
+            trim: true
+        },
+        allotmnetDate: {
+            type: Date,
+            trim: true,
+            get(value) {
+                return moment(value).format("YYYY-MM-DD");
+              }
+        },
+        refundInitiation: {
+            type: Date,
+            trim: true,
+            get(value) {
+                return moment(value).format("YYYY-MM-DD");
+              }
+        },
+        listingOnExchange: {
+            type: Date,
+            trim: true,
+            get(value) {
+                return moment(value).format("YYYY-MM-DD");
+              }
+        },
+        
+        isActive: {
+            type: Boolean,
+            default: true,
+        },
+        isDeleted: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+cmsIpoDatesSchema.virtual('ipoDocS3').get(function () {
+
+ 
+    return  this.ipoDocS3 ? getImageURL(this.ipoDocS3) : null;
+  })
+
+const cmsIpoDates = mongoose.model("cmsIpoDates", cmsIpoDatesSchema);
+module.exports = { cmsIpoDates };

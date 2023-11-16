4daf926ec043228dae55f258b800f43a9c3a58bd
Gowrisankar <102644027+gowrisankardoodleblue@users.noreply.github.com>
2023-11-16 12:42:10 +0530

    Update cmsIpoDates.js

--- app/models/cmsIpoDates.js
+++ app/models/cmsIpoDates.js
@@ -1,58 +1,56 @@
 const mongoose = require("mongoose");
 
-const { getImageURL } = require("../utils/s3Utils")
+const { getImageURL } = require("../utils/s3Utils");
 const moment = require("moment");
 
 const cmsIpoDatesSchema = new mongoose.Schema(
-    {
-        ipoisinNumber: {
-            type: String,
-            trim: true
-        },
-        ipoDoc: {
-            type: String,
-            trim: true
-        },
-        allotmnetDate: {
-            type: Date,
-            trim: true,
-            get(value) {
-                return moment(value).format("YYYY-MM-DD");
-              }
-        },
-        refundInitiation: {
-            type: Date,
-            trim: true,
-            get(value) {
-                return moment(value).format("YYYY-MM-DD");
-              }
-        },
-        listingOnExchange: {
-            type: Date,
-            trim: true,
-            get(value) {
-                return moment(value).format("YYYY-MM-DD");
-              }
-        },
-        
-        isActive: {
-            type: Boolean,
-            default: true,
-        },
-        isDeleted: {
-            type: Boolean,
-            default: false,
-        },
+  {
+    ipoisinNumber: {
+      type: String,
+      trim: true,
     },
-    {
-        timestamps: true,
-    }
-);
-cmsIpoDatesSchema.virtual('ipoDocS3').get(function () {
+    ipoDoc: {
+      type: String,
+      trim: true,
+    },
+    allotmnetDate: {
+      type: Date,
+      trim: true,
+      get(value) {
+        return moment(value).format("YYYY-MM-DD");
+      },
+    },
+    refundInitiation: {
+      type: Date,
+      trim: true,
+      get(value) {
+        return moment(value).format("YYYY-MM-DD");
+      },
+    },
+    listingOnExchange: {
+      type: Date,
+      trim: true,
+      get(value) {
+        return moment(value).format("YYYY-MM-DD");
+      },
+    },
 
- 
-    return  this.ipoDoc ? getImageURL(this.ipoDoc) : null;
-  })
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+cmsIpoDatesSchema.virtual("ipoDocS3").get(function () {
+  return this.ipoDoc ? getImageURL(this.ipoDoc) : null;
+});
 
 const cmsIpoDates = mongoose.model("cmsIpoDates", cmsIpoDatesSchema);
 module.exports = { cmsIpoDates };

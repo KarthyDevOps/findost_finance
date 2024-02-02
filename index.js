92893ffc693aecafbed8a1ec33c753e3437f0f00
kishoreBE <kishore.t@doodleblue.in>
2024-02-02 14:27:32 +0530

    credentials update

--- index.js
+++ index.js
@@ -25,6 +25,10 @@ process.env.CONFIG_ARG = args;
 let CONFIG = require('./app/configs/config')(args)
 process.env = { ...process.env,...CONFIG}
 
+console.log("Ars", args);
+console.log("configs--->", process.env);
+
+
 
 app.use(urlencoded({ extended: false }));
 app.use(cors());
@@ -47,15 +51,37 @@ app.use((req, res, next) => {
   next();
 });
 
-//DB connection
-const connectOptions = {
-  useNewUrlParser: true,
-  autoIndex: true,
-};
-mongoose.connect(process.env.MONGO_URI, connectOptions, (e) =>
-  e ? console.log(e) : console.log("DB connected successfully..")
-);
 
+let mongoDBOptions;
+
+if (args === "PREPROD") {
+  mongoDBOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true,
+    sslValidate: false,
+  };
+} else {
+  mongoDBOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+}
+
+console.log("MOngo DB options ", mongoDBOptions);
+// Connect to database
+mongoose
+  .connect(process.env.MONGO_URI, mongoDBOptions)
+  .then((res) => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log("Database connection error", err);
+  });
+mongoose.connection.on("error", function (err) {
+  console.error("MongoDB connection error: " + err);
+  process.exit(-1);
+});
 const port = process.env.PORT;
 app.use("/finance", routerService);
 
